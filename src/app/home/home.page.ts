7a53edb92733b67e1c97ff7f1231fcd0876234a3
kj1352 <nikhilkj24@gmail.com>
2022-06-07 19:18:43 +0530

    Counter app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
 
+type ICounter = {
+	name: string;
+	startDate: Date;
+};
+
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+	showAddModal = false;
+	newCounter: ICounter;
 
-  constructor() {}
+	allCounters: Array<ICounter> = [];
 
+	constructor() { }
+
+	openModal() {
+		this.showAddModal = true;
+
+		this.newCounter = {
+			name: undefined,
+			startDate: new Date()
+		};
+	}
+
+	getDays(date: Date) {
+		return (new Date().getTime() - new Date(date).getTime()) / (1000 * 3600 * 24);
+	}
+
+	addCounter() {
+		this.allCounters.push(this.newCounter);
+		this.showAddModal = false;
+	}
+
+	relapse(index: number) {
+		this.allCounters[index].startDate = new Date();
+	}
 }
