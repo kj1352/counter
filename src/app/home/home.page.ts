baf9aa531392a62091f45c9cb87978bf8778dc0b
kj1352 <nikhilkj24@gmail.com>
2022-06-07 19:49:01 +0530

    Added option to delete and restore

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 type ICounter = {
 	name: string;
 	startDate: Date;
+	isArchived?: boolean;
 };
 
 @Component({
@@ -13,10 +15,23 @@ type ICounter = {
 export class HomePage {
 	showAddModal = false;
 	newCounter: ICounter;
+	showArchived = false;
 
 	allCounters: Array<ICounter> = [];
 
-	constructor() { }
+	constructor(
+		public alertController: AlertController
+	) {
+		this.allCounters = JSON.parse(localStorage.getItem('counter'));
+
+		if (!this.allCounters) {
+			this.allCounters = [];
+		}
+	}
+
+	getArchivedCount() {
+		return this.allCounters.filter(counter => counter.isArchived).length;
+	}
 
 	openModal() {
 		this.showAddModal = true;
@@ -28,15 +43,50 @@ export class HomePage {
 	}
 
 	getDays(date: Date) {
-		return (new Date().getTime() - new Date(date).getTime()) / (1000 * 3600 * 24);
+		return ((new Date().getTime() - new Date(date).getTime()) / (1000 * 3600 * 24)).toFixed();
 	}
 
 	addCounter() {
 		this.allCounters.push(this.newCounter);
+		localStorage.setItem('counter', JSON.stringify(this.allCounters));
 		this.showAddModal = false;
 	}
 
 	relapse(index: number) {
 		this.allCounters[index].startDate = new Date();
+		localStorage.setItem('counter', JSON.stringify(this.allCounters));
+	}
+
+	deleteCounter(index: number) {
+		this.allCounters[index].isArchived = true;
+		localStorage.setItem('counter', JSON.stringify(this.allCounters));
+	}
+
+	retrieveCounter(index: number) {
+		this.allCounters[index].isArchived = false;
+		localStorage.setItem('counter', JSON.stringify(this.allCounters));
+	}
+
+	async presentAlertConfirm(index: number) {
+		const alert = await this.alertController.create({
+			message: 'Would you like to Relapse or Delete?',
+			buttons: [
+				{
+					text: 'Relapse',
+					handler: () => {
+						console.log('Relapse clicked');
+						this.relapse(index);
+					}
+				}, {
+					text: 'Delete',
+					handler: () => {
+						console.log('Delete clicked');
+						this.deleteCounter(index);
+					}
+				}
+			]
+		});
+
+		await alert.present();
 	}
 }
